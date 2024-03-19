1886236fc1b9c68d05aed56441cd3deda873fae1
Cam Bui <29789422+ncbui@users.noreply.github.com>
2024-03-19 22:31:40 +0000

    Add resume using react-native-markdown-view

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,17 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import ResumeView from '@/components/ResumeView';
+import { ExternalLink } from '@/components/ExternalLink';
+
+const resumeURL = 'https://github.com/ncbui/ncbui.github.io/blob/main/src/index.pdf'
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <ExternalLink href={resumeURL}> download </ExternalLink> */}
+      <ResumeView />
     </View>
   );
 }
---
+++ components/ResumeView.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from 'react-native';
+import { MarkdownView } from 'react-native-markdown-view'
+import { Text, View } from './Themed';
+import { MonoText } from './StyledText';
+import Colors from '@/constants/Colors';
+
+const about = `
+**Cam Bui**
+============
+
+Full stack software dev, devOps, dataOps. Looking for fully remote roles.`
+const experience = `
+
+
+Experience
+----------
+
+**Consultant Software Developer**, Thoughtworks, Inc., United States. 2021 - 2024.
+
+* Collaborated with health and life-sciences teams to build, deploy, and maintain containerized, secure, multi-cloud data products onto existing infrastructure, AWS, and Azure.
+* Reduced time to setup machine-learning experiments with starter-kits to enable cross-functional teams to explore ML model use-cases.
+* Advocated for platform-engineering-centered approach to digital transformation to enhance developer and team productivity.
+* Implemented automated software testing, release, deployment, and compliance and vulnerability checks using git, bash and python scripting, and Github Actions workflows.
+
+**Data Entry**, Invitae Inc., San Francisco, CA. 2017 - 2020.
+
+* Triaged electronic health records to address discrepancies and reduce delays to turn-around time.
+* Streamlined data quality checks by creating Salesforce reports and Tableau dashboards.
+
+**Graduate Researcher**, Action & Consciousness Lab, San Francisco State University. 2016 - 2018.
+
+* Conducted behavioral experiments about experiences of unintended and automatic syntactic processing.
+
+**Health Policy Intern**, Health Promotion & Wellness unit, San Francisco State University. 2016 - 2017.
+
+* Conducted literature reviews and focus groups on student health and wellness needs to assist health center with program planning.
+
+Technical Stack
+--------------------
+
+* Python, React, Ruby on Rails, Jest, Pytest, Mocha
+* Infrastructure as code, Kubernetes, Istio, ArgoCD, GitOps, HELM charting, Vault, Datadog, IAM/RBAC, Snyk, Aqua, GithubActions, Linux, and scripting (Bash and Python, etc.)
+* Pyspark, Pandas, Hadoop, postgresDB, Azure Blob Storage, AWS (S3), Azure Databricks, Unity Catalog, Starburst, SQL
+
+Certifications
+--------------------
+
+* AWS Cloud Practitioner
+
+Education
+---------
+
+**Graduate coursework, Social, Personality, and Affective Psychology**; San Francisco State University (San Francisco, CA), 2016-2019.
+
+* Bui, N. C. T., Ghafur, R. D., Yankulova, J. K., & Morsella, E. (2023). Stimulus-elicited involuntary insights and syntactic processing. Psychology of Consciousness: Theory, Research, and Practice, 10(2), 115–133. https://doi.org/10.1037/cns0000208
+
+**BSc, Kinesiology: Neuromuscular Science**; San Francisco State University (San Francisco, CA), 2016.
+`;
+
+export default function ResumeView() {
+    return (
+         <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.resumeContainer}>
+                <View style={styles.resumeContainer}>
+                    <MarkdownView style={styles.getStartedText}>
+                    {about}
+                    {experience}
+                    </MarkdownView>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+  }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+      },
+      scrollView: {
+        backgroundColor: 'pink',
+        marginHorizontal: 20,
+      },
+    resumeContainer: {
+      alignItems: 'center',
+      marginHorizontal: 100,
+      backgroundColor: '#ffd9da',
+    },
+    getStartedText: {
+      fontSize: 14,
+      lineHeight: 14,
+      textAlign: 'right',
+    },
+  });  
+  
