ad4fe89d6b9c9e2ac2ea7e8ffdc2e9f9ee1153a4
Cam Bui <29789422+ncbui@users.noreply.github.com>
2024-03-19 20:05:45 +0000

    Initiate expo app

---
+++ dev-portfolio/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#ffd9da';
+const tintColorDark = '#1b2021';
+
+export default {
+  light: {
+    text: '#30343f',
+    background: '#ffd9da',
+    tint: tintColorLight,
+    tabIconDefault: '#89023e',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#ffd9da',
+    background: '#30343f',
+    tint: tintColorDark,
+    tabIconDefault: '#1b2021',
+    tabIconSelected: tintColorDark,
+  },
+};
