69e287c9d0ea0015ffec4fe8f21e50240ba2c12b
cb <29789422+ncbui@users.noreply.github.com>
2025-03-23 23:18:24 -0700

    Add newRandom method and color prop to constructor

--- src/components/Game/Point.jsx
+++ src/components/Game/Point.jsx
@@ -1,5 +1,3 @@
-import { theme } from "../../template/theme";
-
 /** Point: a single element on the game board.
  *
  * This is used to draw a circle on the game board at x,y. It is used by both
@@ -11,15 +9,16 @@ import { theme } from "../../template/theme";
  *
  * */
 export default class Point {
-    constructor(p) {
+    constructor(p, color = 'yellow') {
       this.x = p.x;
       this.y = p.y;
+      this.color = color
     }
 
-    draw(ctx, color=theme.palette.primary.bright){
+    draw(ctx, color=this.color){
         ctx.fillStyle = color;
         // Set the border colour of the snake part
-        ctx.strokestyle = theme.palette.primary.headers;
+        ctx.strokestyle = color;
         // Draw a "filled" rectangle to represent the point at the coordinates
         // the part is located
         ctx.fillRect(this.x, this.y, 10, 10);
@@ -30,5 +29,12 @@ export default class Point {
     /** Return t/f if this point is outside of the game board coords. */
     isOutOfBound(width,height) {
         return (this.x <= 0 || this.x >= width || this.y <= 0 || this.y >= height);
+    }
+
+    /** Make a point at a random x,y and return it. */
+
+    static newRandom(width,height, color=this.color) {
+        const randRange = (low, hi) => low + Math.floor(((Math.random() * (hi - low))/10))*10;
+        return new Point({x: randRange(1, width), y: randRange(1, height)},color);
     }
 }
