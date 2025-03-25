54e4932241eed1c8b90c94e7fb39086ca2002ede
cb <29789422+ncbui@users.noreply.github.com>
2025-03-24 22:43:36 -0700

    Expand description

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -14,7 +14,7 @@ export default function Game () {
             <NavTitleText sx={{fontStyle: 'italic', fontWeight:'600', fontSize: '1rem', marginLeft:'1rem', color: theme.palette.text}}>
             Built with React, Vite, HTML Canvas, and requestAnimationFrame.
             </NavTitleText>
-        <Subtitle sx={{maxWidth:'22rem', margin: '1rem'}}>
+        <Subtitle sx={{maxWidth:'80%', margin: '1rem'}}>
             Start game to start hatch a wandering anaconda. This hungry hatchling is drawn towards food. Help them eat and grow safely.
             Press the reset button try to draw the hatchling in a new direction. Eat well. Live long and prosper.
         </Subtitle>
