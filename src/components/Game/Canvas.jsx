9483fdf1a0b70890f090fd8598c405d56f304ff4
cb <29789422+ncbui@users.noreply.github.com>
2025-03-25 12:46:13 -0700

    Extract methods and constants from Canvas

--- src/components/Game/Canvas.jsx
+++ src/components/Game/Canvas.jsx
@@ -1,61 +1,43 @@
-import {  theme, GameButton } from '../../template/theme';
-import { Card, Container, Sheet } from '@mui/joy';
 import { useRef, useState, useLayoutEffect, useEffect } from 'react';
+import { theme, GameButton } from '../../template/theme';
+import { Card, Sheet } from '@mui/joy';
+import { Box } from '@mui/material';
 import FoodSources from './Food';
 import NPC from './NPC';
-import { Box } from '@mui/material';
+import {WIDTH,HEIGHT,SCALE,FPS, drawCanvas, gameOverText} from './constants';
     
-const drawCanvas = ( canvas) => {
-    const { width, height } = canvas.getBoundingClientRect()
-    const ctx = canvas.getContext('2d')
-    ctx.save()
-    ctx.clearRect(0, 0, width, height)
-}
-
-const WIDTH = 25;
-const HEIGHT = 20;
-const SCALE = 15;
-const FPS = 5;
-
 
 export default function Canvas() { 
-    const canvasRef = useRef()     // get canvas
+    const canvasRef = useRef()
     const [shouldStart, setShouldStart] = useState(false)
     const [frameCounter, setFrameCounter] = useState(0)
-    const [snake, setSnake] = useState(new NPC())
+    const [caterpillar, setCaterpillar] = useState(new NPC())
     const [food, setFood] = useState(new FoodSources())
-    // output graphics, re-renders when update changes
+
     useEffect(() => {
         const canvas = canvasRef.current
         const {width,height} = canvas
         const context = canvas.getContext('2d')
         
         if (shouldStart){
-            drawCanvas(canvas)
+            drawCanvas(context)
             food.refillFood()
             food.draw(context)
-            // if gameend: out of Bounds, hit itself, hit others, game OVER
-            if (snake.gameOver()){
-                snake.draw(context)
-                context.textAlign = 'center'
-                context.fillStyle = 'white'
-                context.font = '34px serif'
-                context.fillText('ouch', ((WIDTH * SCALE) / 2), ((HEIGHT * SCALE) / 2 - (2 * SCALE)))
-                context.font = '18px serif'
-                context.fillText(`caterpillar hurt itself in its confusion`, ((WIDTH * SCALE) / 2), ((HEIGHT * SCALE) / 2 + (2 * SCALE)))
-                setSnake(new NPC())
+            if (caterpillar.gameOver()){
+                caterpillar.draw(context)
+                gameOverText(context)
+                setCaterpillar(new NPC())
                 setFrameCounter(0)
                 setShouldStart(false)
                 return () => {}
             }
-            snake.move(food,setFood,width,height)
-            snake.draw(context)
-            food.refillFood()
+            caterpillar.move(food,setFood)
+            caterpillar.draw(context)
             context.restore()
         }
         return () => {
         }
-    }, [frameCounter, snake, shouldStart, food])
+    }, [frameCounter, caterpillar, shouldStart, food])
 
     useLayoutEffect(() => {
         if (shouldStart) {
@@ -77,12 +59,12 @@ export default function Canvas() {
                 <GameButton id='startButton' onClick={() => setShouldStart(!shouldStart)} sx={{width:'fit-content', m:'1rem'}} > 
                     { shouldStart? 'Stop' : 'Start'}
                 </GameButton>
-                <GameButton id='foodButton' onClick={() => setFood(food.resetFood())} sx={{width:'fit-content', m:'1rem'}} > 
+                <GameButton id='foodButton' onClick={() => food.resetFood()} sx={{width:'fit-content', m:'1rem'}} > 
                     Reset Food
                 </GameButton>
             </Box>
             <Sheet sx={{backgroundColor:theme.palette.background, borderRadius:'.3rem', border: '3px solid', borderColor: theme.palette.text, display:'flex', flexDirection: 'column', justifyItems:'center', alignItems:'center'}}>
-                <canvas id='snakeboard' ref={canvasRef} width={WIDTH*SCALE} height={HEIGHT*SCALE} style={{}}/>
+                <canvas id='caterpillarboard' ref={canvasRef} width={WIDTH*SCALE} height={HEIGHT*SCALE} style={{}}/>
             </Sheet>
         </Card>
     )
--- src/components/Game/constants.jsx
+++ src/components/Game/constants.jsx
@@ -2,3 +2,17 @@ export const WIDTH = 25;
 export const HEIGHT = 20;
 export const SCALE = 15;
 export const FPS = 5;
+
+export const gameOverText = (context) => {
+    context.textAlign = 'center'
+    context.fillStyle = 'white'
+    context.font = '34px serif'
+    context.fillText('ouch', ((WIDTH * SCALE) / 2), ((HEIGHT * SCALE) / 2 - (2 * SCALE)))
+    context.font = '18px serif'
+    context.fillText(`caterpillar bit itself in its confusion`, ((WIDTH * SCALE) / 2), ((HEIGHT * SCALE) / 2 + (2 * SCALE)))
+}
+
+export const drawCanvas = ( context) => {
+    context.save()
+    context.clearRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE)
+}
