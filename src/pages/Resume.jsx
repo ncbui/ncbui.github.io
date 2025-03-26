746b432c7ba813cdd8e658a140ed5916610a7791
cb <29789422+ncbui@users.noreply.github.com>
2025-03-25 20:17:00 -0700

    disable build button

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -112,7 +112,7 @@ export default function Resume() {
     <WorkSheets component={Paper} sx={{ height:'100vh', justifyContent: 'center' }} >
        <ResumeContainer component={Paper} sx={{padding: '1rem 0'}}>
         <ResumeFab variant="extended" onClick={downloadPdf}>Download</ResumeFab>
-        <ResumeFab variant="extended" onClick={buildPdf}>Build</ResumeFab>
+        <ResumeFab variant="extended" onClick={buildPdf} disabled>Build</ResumeFab>
         <Table id='container' sx={{ minWidth: 450}} size="medium" aria-label="a dense table">
           <TableHead>
             { makeTableHead() }
