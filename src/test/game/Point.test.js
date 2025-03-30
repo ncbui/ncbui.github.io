ae7c478d670ee3b7da5a5bbb6760ffb7364ec3ae
cb <29789422+ncbui@users.noreply.github.com>
2025-03-30 15:25:29 -0700

    Add test for OutOfBounds

--- src/test/game/Point.test.js
+++ src/test/game/Point.test.js
@@ -13,7 +13,7 @@ describe('Point class', () => {
       expect(point.y).toBe(20);
       expect(point.color).toBe('red');
     });
-    
+
     it('draw() sets styles and calls canvas methods', () => {
         const ctx = {
           fillStyle: '',
@@ -29,4 +29,16 @@ describe('Point class', () => {
         expect(ctx.fillRect).toHaveBeenCalledWith(20, 30, 10, 10);
         expect(ctx.strokeRect).toHaveBeenCalledWith(20, 30, 2, 1);
       });
+
+    it('isOutOfBound() detects out-of-bounds correctly', () => {
+    const inBoundsPoint = new Point({ x: 50, y: 50 });
+    const edgeOfBoundsPoint = new Point({ x: 10, y: HEIGHT });
+    const outOfBoundsPoint = new Point({ x: -5, y: -200 });
+    const outOfBoundsPointTwo = new Point({ x: -1*HEIGHT, y: HEIGHT });
+
+    expect(inBoundsPoint.isOutOfBound()).toBe(false);
+    expect(edgeOfBoundsPoint.isOutOfBound()).toBe(false);
+    expect(outOfBoundsPoint.isOutOfBound()).toBe(true);
+    expect(outOfBoundsPointTwo.isOutOfBound()).toBe(true);
+    });
 })
