9f0bc50b2e5b12eb47cf598ff549534dbe7a5f4f
cb <29789422+ncbui@users.noreply.github.com>
2025-04-25 14:00:00 -0700

    Mirror lists in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,36 +1,30 @@
-import { Typography } from '@mui/joy';
+import { Typography} from '@mui/joy';
+import { List, ListItem, ListItemText, ListSubheader } from '@mui/material';
 import { BodySheets } from '../template/theme';
 
 export default function About() {
   return (
     <BodySheets>
       <Typography sx={{color:'inherit'}}> Hi, I'm Cam. I build cloud native software and pipelines.  </Typography>
-      <Typography variant="title" color="inherit" noWrap>
-        &nbsp;
-      </Typography>
-      <Typography sx={{color:'inherit'}}>
-        Recently, I was a software consultant at Thoughtworks.
-        </Typography>
-      <Typography variant="title" color="inherit" noWrap>
-        &nbsp;
-      </Typography>
-      <Typography variant="title" color="inherit" noWrap>
-        &nbsp;
-      </Typography>
-      <Typography sx={{color:'inherit'}}>
-        I also
-        <List>
+      <List dense={true}>
           <ListItem>
-            <ListItemText primary="can be found building soil in gardens" />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="draw, paint, and sculpt" />
-          </ListItem>
-          <ListItem>
-            <ListItemText primary="boulder indoors and " />
+            <ListItemText primary="Recently, I was a software consultant at Thoughtworks." />
           </ListItem>
         </List>
-         </Typography>
+      <Typography color="inherit">
+        I also
+      </Typography>
+        <List dense={true}>
+          <ListItem>
+            <ListItemText primary="practice aerial arts, indoor bouldering, and taiji." />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="can be found building soil in gardens." />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="draw, paint, and sculpt." />
+          </ListItem>
+        </List>
     </BodySheets>
   );
 }
